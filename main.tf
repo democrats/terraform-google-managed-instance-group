85c9eeeaf4931d3da6202ff6b81eb7e218ddc391
Joshua Carp <jm.carp@gmail.com>
2019-08-27 14:12:21 -0400

    Fix compatibliity with provider 2.x.

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "google_compute_instance_template" "default" {
     network            = "${var.subnetwork == "" ? var.network : ""}"
     subnetwork         = "${var.subnetwork}"
     access_config      = ["${var.access_config}"]
-    address            = "${var.network_ip}"
+    network_ip         = "${var.network_ip}"
     subnetwork_project = "${var.subnetwork_project == "" ? var.project : var.subnetwork_project}"
   }
 
@@ -68,6 +68,7 @@ resource "google_compute_instance_template" "default" {
 }
 
 resource "google_compute_instance_group_manager" "default" {
+  provider           = "google-beta"
   count              = "${var.module_enabled && var.zonal ? 1 : 0}"
   project            = "${var.project}"
   name               = "${var.name}"
@@ -76,13 +77,14 @@ resource "google_compute_instance_group_manager" "default" {
 
   base_instance_name = "${var.name}"
 
-  instance_template = "${google_compute_instance_template.default.self_link}"
+  version {
+    name              = "default"
+    instance_template = "${google_compute_instance_template.default.self_link}"
+  }
 
   zone = "${var.zone}"
 
-  update_strategy = "${var.update_strategy}"
-
-  rolling_update_policy = ["${var.rolling_update_policy}"]
+  update_policy = ["${var.rolling_update_policy}"]
 
   target_pools = ["${var.target_pools}"]
 
